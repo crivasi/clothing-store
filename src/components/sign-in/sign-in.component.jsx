7839e5cc20ad342fed4b47e6986f619f59a811e5
Christian Rivas <christian.x.rivas.-nd@disney.com>
2019-10-10 15:56:51 -0500

    finishing sign in handleSubmit mehotd

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 import "./sign-in.styles.scss";
 class SignIn extends React.Component {
@@ -17,7 +17,13 @@ class SignIn extends React.Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+    try {
+      auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   handleChange = event => {
